227dde529d39e6f7536fb4abe7ad6ac029dfeea2
SnowAqua55 <63825945+SnowAqua55@users.noreply.github.com>
2025-05-26 21:20:39 +0900

    feat : Player Look
    플레이어 움직임 (점프 포함) 구현 완료
    카메라 제작 중

--- Assets/1. Scripts/Player/Player.cs
+++ Assets/1. Scripts/Player/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -6,12 +7,23 @@ public class Player : MonoBehaviour
     [Header("Moving")]
     public float moveSpeed;
     public float jumpForce;
-    private Vector2 moveInput;
+    private Vector2 _moveInput;
     public Rigidbody rigidbody;
+    // public LayerMask groundLayerMask;
+    // public float groundRayDistance;
+    private bool _isGround;
+
+    [Header("Looking")]
+    public float sensitive;
+    
+    private Camera _camera;
+    
     
     private void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
+        _camera = Camera.main;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 	
     private void FixedUpdate()
@@ -21,21 +33,55 @@ public class Player : MonoBehaviour
 
     private void Move()
     {
-        Vector3 move = new Vector3(moveInput.x, 0, moveInput.y);
-        rigidbody.MovePosition(this.transform.position + move * moveSpeed * Time.deltaTime);
+        Vector3 move = new Vector3(_moveInput.x, 0, _moveInput.y);
+        float moveTime = moveSpeed * Time.deltaTime;
+        rigidbody.MovePosition(transform.position + move * moveTime);
     }
     
     public void OnMove(InputAction.CallbackContext context)
     {
         if (context.phase == InputActionPhase.Performed)
-            moveInput = context.ReadValue<Vector2>();
+            _moveInput = context.ReadValue<Vector2>();
         else if (context.phase == InputActionPhase.Canceled)
-            moveInput = Vector2.zero;
+            _moveInput = Vector2.zero;
     }
 
     public void OnJump(InputAction.CallbackContext context)
     {
-        if (context.started)
+        if (context.started && _isGround)
             rigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+    }
+
+    // private bool IsGround()
+    // {
+    //     Ray[] rays = new Ray[4]
+    //     {
+    //         new Ray(transform.position + (transform.forward * 0.2f) + (transform.up * 0.01f), Vector3.down * 100.0f),
+    //         new Ray(transform.position + (-transform.forward * 0.2f) + (transform.up * 0.01f), Vector3.down * 100.0f),
+    //         new Ray(transform.position + (transform.right * 0.2f) + (transform.up * 0.01f), Vector3.down * 100.0f),
+    //         new Ray(transform.position + (-transform.right * 0.2f) + (transform.up * 0.01f), Vector3.down * 100.0f)
+    //     };
+    //
+    //     for (int i = 0; i < rays.Length; i++)
+    //     {
+    //         if (Physics.Raycast(rays[i], groundRayDistance, groundLayerMask))
+    //         {
+    //             return true;
+    //         }
+    //     }
+    //
+    //     return false;
+    // }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+            _isGround = true;
+    }
+
+    private void OnCollisionExit(Collision other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+            _isGround = false;
     }
 }
