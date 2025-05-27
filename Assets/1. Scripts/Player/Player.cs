4f08e64180120d2404b5f915fad696a52de63426
SnowAqua55 <63825945+SnowAqua55@users.noreply.github.com>
2025-05-27 20:04:50 +0900

    feat : Character Info
    캐릭터 스탯 구현 중

--- Assets/1. Scripts/Player/Player.cs
+++ Assets/1. Scripts/Player/Player.cs
@@ -1,13 +1,14 @@
-using System;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     public PlayerController controller;
+    public CharacterInfo info;
 
     private void Awake()
     {
         CharacterManager.Instance.Player = this;
         controller = GetComponent<PlayerController>();
+        info = GetComponent<CharacterInfo>();
     }
 }
--- Assets/1. Scripts/Player/PlayerController.cs
+++ Assets/1. Scripts/Player/PlayerController.cs
@@ -7,26 +7,35 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed;
     public float jumpForce;
     private Vector2 _moveInput;
-    public Rigidbody rigidbody;
+    private Rigidbody _rigidbody;
+    private Vector3 _direction;
     // public LayerMask groundLayerMask;
     // public float groundRayDistance;
+    
     private bool _isGround;
+    private int _isRun;
+    private int _isJump;
 
     [Header("Looking")]
     public Transform cameraContainer;
     public float sensitive;
-    private Vector2 mouseDelta;
-    private float camCurXRot;
-    
+    private Vector2 _mouseDelta;
+    private float _camCurXRot;
+
+    private Animator _animator;
     
     private void Start()
     {
-        rigidbody = GetComponent<Rigidbody>();
+        _rigidbody = GetComponent<Rigidbody>();
+        _animator = GetComponent<Animator>();
         cameraContainer = transform.Find("Camera Container");
+
+        _isRun = Animator.StringToHash("isRun");
+        _isJump = Animator.StringToHash("isJump");
         
         Cursor.lockState = CursorLockMode.Locked;
     }
-	
+
     private void FixedUpdate()
     {
         Move();
@@ -39,45 +48,58 @@ public class PlayerController : MonoBehaviour
 
     private void Move()
     {
-        Vector3 moveInput = new Vector3(_moveInput.x, 0, _moveInput.y);
-        Vector3 worldMove = transform.TransformDirection(moveInput);
-        if (worldMove.magnitude > 1)
-            worldMove.Normalize();
-        Vector3 moveVelocity = worldMove * moveSpeed;
-        rigidbody.velocity = new Vector3(moveVelocity.x, rigidbody.velocity.y, moveVelocity.z);
+        _direction = transform.forward * _moveInput.y + transform.right * _moveInput.x;
+        _direction *= moveSpeed;
+        _direction.y = _rigidbody.velocity.y;
+        
+        _rigidbody.velocity = _direction;
+        
+        // Vector3 moveInput = new Vector3(_moveInput.x, 0, _moveInput.y);
+        // Vector3 worldMove = transform.TransformDirection(moveInput);
+        // if (worldMove.magnitude > 1)
+        //     worldMove.Normalize();
+        // Vector3 moveVelocity = worldMove * moveSpeed;
+        // rigidbody.velocity = new Vector3(moveVelocity.x, rigidbody.velocity.y, moveVelocity.z);
+
         // float moveTime = moveSpeed * Time.deltaTime;
         // rigidbody.MovePosition(transform.position + moveInput * moveTime);
     }
 
     private void Look()
     {
-        camCurXRot += mouseDelta.y * sensitive;
-        camCurXRot = Mathf.Clamp(camCurXRot, -90, 90);
-        cameraContainer.localEulerAngles = new Vector3(-camCurXRot, 0, 0);
+        _camCurXRot += _mouseDelta.y * sensitive;
+        _camCurXRot = Mathf.Clamp(_camCurXRot, -90, 90);
+        cameraContainer.localEulerAngles = new Vector3(-_camCurXRot, 0, 0);
         
-        transform.eulerAngles += new Vector3(0, mouseDelta.x * sensitive, 0);
+        transform.eulerAngles += new Vector3(0, _mouseDelta.x * sensitive, 0);
     }
     
     public void OnMove(InputAction.CallbackContext context)
     {
         if (context.phase == InputActionPhase.Performed)
+        {
             _moveInput = context.ReadValue<Vector2>();
+            _animator.SetBool(_isRun, true);
+        }
         else if (context.phase == InputActionPhase.Canceled)
+        {
             _moveInput = Vector2.zero;
+            _animator.SetBool(_isRun, false);
+        }
     }
 
     public void OnLook(InputAction.CallbackContext context)
     {
-        mouseDelta = context.ReadValue<Vector2>();
+        _mouseDelta = context.ReadValue<Vector2>();
     }
 
     public void OnJump(InputAction.CallbackContext context)
     {
         if (context.started && _isGround)
-            rigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            _rigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
     }
 
-    // IsGround Ray
+    // bool IsGround Ray
     // private bool IsGround()
     // {
     //     Ray[] rays = new Ray[4]
@@ -103,11 +125,13 @@ public class PlayerController : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Ground"))
             _isGround = true;
+        _animator.SetBool(_isJump, false);
     }
 
     private void OnCollisionExit(Collision other)
     {
         if (other.gameObject.CompareTag("Ground"))
             _isGround = false;
+        _animator.SetBool(_isJump, true);
     }
 }
