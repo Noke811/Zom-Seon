1dc26e07116df8b25969294faf754f3f889bf16f
yj6662 <yj6662@naver.com>
2025-05-29 21:27:18 +0900

    [feat] WorldMap
    -feat WorldMap 월드맵 변경

--- Assets/1. Scripts/Architect/ArchitectData.cs
+++ Assets/1. Scripts/Architect/ArchitectData.cs
@@ -2,14 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class MaterialCost
+{
+    public ItemData item;
+    public int amount;
+}
 [CreateAssetMenu(fileName = "Architect", menuName = "new Architect")]
 public class ArchitectData : ScriptableObject
 {
+    // interface IDamageable 적용 필요
     public string itemName;
     public Sprite itemSprite;
     public GameObject itemPrefab;
     public GameObject itemPreviewPrefab;
-    public Dictionary<string, int> requiredMaterials;
+    public List<MaterialCost> materialCosts;
     public bool isPlaceable;
     public bool isTool;
     public Material originalMaterial;
